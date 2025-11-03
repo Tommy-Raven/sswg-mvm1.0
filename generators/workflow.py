2d17800498d2ccdd622112e875f82724a1312d28
Tommy Raven <163503078+Tommy-Raven@users.noreply.github.com>
2025-11-03 07:27:19 -0800

    Update workflow.py

--- generators/workflow.py
+++ generators/workflow.py
@@ -1,1 +1,7 @@
-
+#Handles each phase method:
+#run_phase_1 – Initialization
+#run_phase_1_5 – Objective Refinement
+#run_phase_2 – Human-readable How-To
+#run_phase_3 – Modularization
+#run_phase_4 – Evaluation
+#run_phase_5 – Regeneration
